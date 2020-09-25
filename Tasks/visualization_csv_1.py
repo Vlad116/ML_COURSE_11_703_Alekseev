8c4c6f87b6040e11b41e6b1260368ed91e8a2bef
Vladislav <vladalekseev9@yandex.ru>
2020-09-25 07:44:03 +0300

    :sparkles: Init repository and add 1st task!

---
+++ Tasks/visualization_csv_1.py
@@ -0,0 +1,27 @@
+from collections import Counter
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+
+def count_age(test_dataset, survived_dataset):
+    """Return count of passangers. Key is an age of person."""
+    return dict(Counter([round(value) for value in test_dataset.Age if not math.isnan(value)]))
+
+
+def show_count_age():
+    test_dataset = pd.read_csv("../datasets/co2_emission.csv")
+    survived_dataset = pd.read_csv("../datasets/co2_emission.csv")
+
+    age_count = count_age(test_dataset, survived_dataset)
+
+    plt.xlabel('Age')
+    plt.ylabel('Count')
+
+    plt.scatter(age_count.keys(), age_count.values(), c='blue')
+    plt.show()
+
+
+if __name__ == '__main__':
+    show_count_age()
